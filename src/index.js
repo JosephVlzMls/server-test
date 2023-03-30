fd6f91a04fb23f21f1aafca243c861b0e1754427
Joseph Velazquez <jvelazquez@sisu.mx>
2023-03-30 00:29:50 -0600

    segunda versión

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ const server = http.createServer((request, response) => {
     // https://freeipapi.com/api/json/{ip}
     const json = {
         httpVersion: request.httpVersion,
-        remoteAddress: request.socket.remoteAddress,
         method: request.method,
         url: request.url,
+        request: {
+            remoteAddress: request.socket.remoteAddress,
+            address: request.socket.address(),
+        },
+        response: {
+            remoteAddress: response.socket.remoteAddress,
+            address: response.socket.address(),
+        },
         headers: request.headers
     }
+    // response.writeHead(301, {'Location': 'https://www.google.com'})
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.write(JSON.stringify(json))
     response.end()
